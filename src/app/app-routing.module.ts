d8622e00242e4c38a67aa68909a0369268917e71
wnexous <andreneexus@hotmail.com>
2023-08-07 21:22:22 -0300

    create index page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
+import { IndexComponent } from './index/index.component';
 
 const routes: Routes = [
+  { path: "", component: IndexComponent },
   { path: "signin", component: SigninComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SigninComponent } from './signin/signin.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SigninComponent
+    SigninComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
