83c57df849b83def74d10c51147f29f6ba501d21
wnexous <andreneexus@hotmail.com>
2023-08-07 21:50:58 -0300

    clean project and remove not usable components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SigninComponent } from './signin/signin.component';
 import { IndexComponent } from './index/index.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { MatCardModule } from '@angular/material/card';
-import { Component } from '@angular/core';
-
-// @NgModule({
-//   declarations: [
-//     AppComponent
-//   ],
-//   selector: 'menu-overview-example',
-//   templateUrl: 'menu-overview-example.html',
-//   standalone: true,
-//   imports: [
-//     BrowserModule,
-//     AppRoutingModule,
-//     BrowserAnimationsModule,
-//     MatButtonModule,
-//     MatMenuModule
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
 
 @NgModule({
   declarations: [
@@ -37,8 +16,8 @@ import { Component } from '@angular/core';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, 
-    MatCardModule
+    AppRoutingModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
