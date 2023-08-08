f4e6d1b208c9a8d80b6e523defebf1868d12c256
abressam <amandabressam@gmail.com>
2023-08-07 21:23:44 -0300

    include material icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
-import { NgModule } from '@angular/core';
+// import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+
+import {MatCardModule} from '@angular/material/card';
+import {Component} from '@angular/core';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent
+//   ],
+//   selector: 'menu-overview-example',
+//   templateUrl: 'menu-overview-example.html',
+//   standalone: true,
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     BrowserAnimationsModule,
+//     MatButtonModule,
+//     MatMenuModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+
+@Component({
+  selector: 'card-overview-example',
+  templateUrl: 'index.html',
+  standalone: true,
+  imports: [MatCardModule],
 })
-export class AppModule { }
+export class CardOverviewExample {}
