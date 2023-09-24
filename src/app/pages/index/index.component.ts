9fe7385198c8e084cdf122fc9d03737618a07931
abressam <amandabressam@gmail.com>
2023-09-24 17:41:39 -0300

    Adicionando componente de card

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss'],
+  standalone: true,
+  imports: [MatButtonModule, MatDividerModule, MatIconModule, MatCardModule],
 
 })
 export class IndexComponent {
