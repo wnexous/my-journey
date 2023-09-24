7901ddc9247c1784b0a189e15a16f63b421f9c52
wnexous <andreneexus@hotmail.com>
2023-09-23 23:27:40 -0300

    end loading component

--- src/app/components/loading/loading.component.ts
+++ src/app/components/loading/loading.component.ts
@@ -4,7 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-loading',
   templateUrl: './loading.component.html',
   styleUrls: ['./loading.component.scss'],
- 
+
+
 })
 export class LoadingComponent {
 
