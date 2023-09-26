5cbe657b6c83987193a97279e5a0c68b791a1523
abressam <amandabressam@gmail.com>
2023-09-26 09:32:49 -0300

    Atualizando identação

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -3,14 +3,12 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
-
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss'],
   standalone: true,
-  imports: [MatButtonModule, MatDividerModule, MatIconModule, MatCardModule],
-
+  imports: [MatButtonModule, MatDividerModule, MatIconModule, MatCardModule]
 })
 export class IndexComponent {
 
