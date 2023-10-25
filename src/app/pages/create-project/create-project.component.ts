6e9d7ff333af9a3a08eede39eba076589d5b4812
abressam <amandabressam@gmail.com>
2023-10-25 10:23:31 -0300

    Adicionando OnSubmit

--- src/app/pages/create-project/create-project.component.ts
+++ src/app/pages/create-project/create-project.component.ts
@@ -1,25 +1,21 @@
-import {Component} from '@angular/core';
-
-import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {NgIf} from '@angular/common';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-
-import {MatIconModule} from '@angular/material/icon';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatButtonModule} from '@angular/material/button';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-create-project',
   templateUrl: './create-project.component.html',
   styleUrls: ['./create-project.component.scss'],
-  standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, NgIf, MatButtonModule, MatTooltipModule, MatIconModule],
 })
-export class CreateProjectComponent {
-  isNotEmpty = new FormControl('', [Validators.requiredTrue]);
 
-  onSubmit() {
-    // Lógica para processar o envio do formulário aqui
+export class CreateProjectComponent {
+
+  constructor(private http:HttpClient){}
+
+  onSubmit(data: {title: string}) {
+    console.log(data);
+    this.http.post('#', data)
+    .subscribe((res) => {
+      console.log(res)
+    });
   }
 }
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
+  constructor(private http:HttpClient){}
 
+  onSubmit(data: {name: string, email: string, password: string}) {
+    console.log(data);
+    this.http.post('#', data)
+    .subscribe((res) => {
+      console.log(res)
+    });
+  }
 }
