3eb666fd28ee7e2decebee469452d15bedb6432a
abressam <amandabressam@gmail.com>
2023-11-06 20:44:07 -0300

    Feature: método post concluído

--- src/app/pages/user-resume/user-resume.component.ts
+++ src/app/pages/user-resume/user-resume.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
-import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {Validators, FormsModule, ReactiveFormsModule, FormGroup, FormBuilder} from '@angular/forms';
 import {NgIf} from '@angular/common';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -12,20 +13,52 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatNativeDateModule} from '@angular/material/core';
 
+import { HttpClient } from '@angular/common/http';
 @Component({
-  selector: 'app-user-resume',
+  selector: 'user-resume',
   templateUrl: './user-resume.component.html',
   styleUrls: ['./user-resume.component.scss'],
   standalone: true,
   imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, NgIf, MatButtonModule, MatTooltipModule, MatIconModule, MatDatepickerModule, MatNativeDateModule],
 })
 export class UserResumeComponent {
-  nameFormControl = new FormControl('', [Validators.required]);
-  phoneFormControl = new FormControl('', [Validators.required]);
-  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  createResumeForm: FormGroup;
 
-  onSubmit() {
-    
+  constructor(
+    private formBuilder: FormBuilder, 
+    private http: HttpClient) {
+    this.createResumeForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      birthdate: ['', Validators.required],
+      street: ['', Validators.required],
+      streetNumber: ['', Validators.required],
+      district: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      professionalObjective: [''],
+      professionalExperience: [''],
+      coursesAndCertifications: [''],
+      languages: [''],
+    });
   }
 
+  OnSubmit() {
+    if (this.createResumeForm.valid) {
+      const formData = this.createResumeForm.value;
+      const datePipe = new DatePipe('pt-BR');
+      formData.birthdate = datePipe.transform(formData.birthdate, 'dd/MM/yyyy');
+
+      console.log(formData)
+      this.http.post('#', formData).subscribe(
+        (response) => {
+          console.log('Requisição POST bem-sucedida', response);
+        },
+        (error) => {
+          console.error('Erro na requisição POST', error);
+        }
+      );
+    }
+  }
 }
