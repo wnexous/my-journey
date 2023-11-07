1ee049ea0a75032d0f2706bc02eef052ca4d6610
abressam <amandabressam@gmail.com>
2023-11-07 17:03:23 -0300

    Feature: adicionando mat componente e https

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,18 +1,39 @@
+import { SignupService } from './../../service/signup.service';
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Validators, FormsModule, ReactiveFormsModule, FormGroup, FormBuilder} from '@angular/forms';
+import {NgIf} from '@angular/common';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
+  standalone: true,
+  imports: [ReactiveFormsModule, FormsModule, NgIf, MatInputModule, MatFormFieldModule]
 })
 export class SignupComponent {
-  constructor(private http:HttpClient){}
+  signupForm: FormGroup;
 
-  onSubmit(data: {name: string, email: string, password: string}) {
-    console.log(data);
-    this.http.post('#', data)
-    .subscribe((res) => {
-      console.log(res)
-    });
+  constructor(
+    private formBuilder: FormBuilder,
+    private signupService: SignupService)
+    {
+      this.signupForm = this.formBuilder.group({
+        name: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required],
+      });
+    }
+
+  onSubmit() {
+    if(this.signupForm.valid) {
+      const params = this.signupForm.value;
+
+      this.signupService.signupUser(params).subscribe((resp) => {
+        alert('Formulário enviado');
+      });
+    } else {
+      console.log("Erro ao enviar");
+    }
   }
 }
