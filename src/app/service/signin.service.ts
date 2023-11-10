e8bfa81f0d26ebb8e55ece8a4895a9629b3b4c2c
abressam <amandabressam@gmail.com>
2023-11-10 11:44:19 -0300

    Feature: criando service signin

---
+++ src/app/service/signin.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SigninService {
+
+  constructor(
+    private httpClient: HttpClient,
+  ) { }
+  public signinUser(query: any) {
+    const params = new HttpParams()
+    .set('email', query.email)
+    .set('password', query.password)
+
+    console.log('/api/login?' + params.toString())
+    return this.httpClient.post('http://localhost:5500/api/login?' + params.toString(), {});
+  }
+}
