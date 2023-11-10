b05e3c8899fed8599ef3ed0712b26dbed414ec20
abressam <amandabressam@gmail.com>
2023-11-10 11:43:40 -0300

    Feature: adicionando comunicação signin service

--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -1,20 +1,53 @@
+import { SigninService } from 'src/app/service/signin.service';
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Validators, FormsModule, ReactiveFormsModule, FormGroup, FormBuilder} from '@angular/forms';
+import {NgIf} from '@angular/common';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.scss'],
-  
+  standalone: true,
+  imports: [ReactiveFormsModule, FormsModule, NgIf, MatInputModule, MatFormFieldModule]
 })
 export class SigninComponent {
-  constructor(private http:HttpClient){}
+  signinForm: FormGroup;
 
-  onSubmit(data: {email: string, password: string}) {
-    console.log(data);
-    this.http.post('#', data)
-    .subscribe((res) => {
-      console.log(res)
-    });
+  constructor(
+    private formBuilder: FormBuilder,
+    private signinService: SigninService,
+    private router:Router)
+    {
+      this.signinForm = this.formBuilder.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required],
+      });
+    }
+
+  onSubmit() {
+    if(this.signinForm.valid) {
+      const params = this.signinForm.value;
+
+      this.signinService.signinUser(params).subscribe(
+        (resp) => {
+        if(resp) {
+          alert('Login realizado com sucesso!');
+          this.router.navigate(['/']) // adicionar a página home com usuário logado quando houver
+        }
+      },
+        (error) => {
+          if (error.status === 500) {
+            alert('Erro interno do servidor. Usuário não encontrado.');
+          } else {
+            alert('Erro desconhecido. Por favor, tente novamente mais tarde.');
+          }
+        });
+    } else {
+      console.log('Erro ao enviar o formulário');
+    }
   }
 }
+
