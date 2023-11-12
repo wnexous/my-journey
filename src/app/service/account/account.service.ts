260f38c314ae22a5a0adc4b807f75f534ecd7da2
abressam <amandabressam@gmail.com>
2023-11-12 11:20:01 -0300

    feature: criando service para validar se o usuário está logado

---
+++ src/app/service/account/account.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  public isLoggedIn = false;
+
+  constructor() { }
+
+  public Login() {
+    const token = window.localStorage.getItem('token')
+    const uuid: any = window.localStorage.getItem('uuid')
+
+    if(token && uuid) {
+      this.isLoggedIn = true;
+    }
+  }
+}
