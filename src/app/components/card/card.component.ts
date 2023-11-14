e752b07646353792d0efb5842ce334f03b62e492
abressam <amandabressam@gmail.com>
2023-11-14 17:01:38 -0300

    feat: added card component

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss'],
+  standalone: true,
+  imports: [MatCardModule, MatButtonModule, MatIconModule],
+})
+export class CardComponent {
+
+}
