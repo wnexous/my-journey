17ad5acc57d40201370ea6eeecffc07fa364aa6b
abressam <amandabressam@gmail.com>
2023-11-15 20:22:13 -0300

    feat: added update and delete function

--- src/app/service/project/upload.service.ts
+++ src/app/service/project/upload.service.ts
@@ -54,4 +54,55 @@ private params(query: any): HttpParams {
   return this.httpClient.post('http://localhost:5500/api/project?' + params.toString(), image, httpOptions);
 }
 
+public updateFile(title: string, description: string, image: string) {
+
+  const params = new HttpParams()
+  .set('title', title)
+  .set('description', description)
+
+  const token = window.localStorage.getItem('token')
+  const uuid: any = window.localStorage.getItem('uuid')
+
+  const httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'multipart/form-data',
+      'Authorization': 'Bearer ' + token,
+      'X-Request-Id': uuid,
+    })
+  };
+
+  if (!image) {
+    // Um arquivo de imagem foi carregado
+    image = window.localStorage.getItem('image') || ''
+  }
+
+  console.log({
+    title,
+    description,
+    image
+  })
+
+  console.log('AQUI :D')
+
+  return this.httpClient.post('http://localhost:5500/api/project?' + params.toString(), image, httpOptions);
+
+}
+
+public deleteFile() {
+
+  const token = window.localStorage.getItem('token')
+  const uuid: any = window.localStorage.getItem('uuid')
+  const projectId = window.localStorage.getItem('projectId')
+
+  const httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'multipart/form-data',
+      'Authorization': 'Bearer ' + token,
+      'X-Request-Id': uuid,
+    })
+  };
+
+  return this.httpClient.delete('http://localhost:5500/api/project?id=' + projectId, httpOptions)
+}
+
 }
