f786e802d073f2ec14ce5b0520b93ff899925201
adnegro <andre.negro@oi.net.br>
2023-11-14 23:23:23 -0300

    fix image base64 converter

--- src/app/pages/create-project/create-project.component.ts
+++ src/app/pages/create-project/create-project.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 
-import {Validators, FormsModule, ReactiveFormsModule, FormGroup, FormBuilder} from '@angular/forms';
-import {NgIf} from '@angular/common';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { Validators, FormsModule, ReactiveFormsModule, FormGroup, FormBuilder } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
-import {MatIconModule} from '@angular/material/icon';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
 
 import { UploadService } from 'src/app/service/project/upload.service';
 
@@ -21,11 +21,10 @@ import { UploadService } from 'src/app/service/project/upload.service';
 
 export class CreateProjectComponent {
   createProjetForm: FormGroup;
-
-  file: File | null = null;
   selectedFileName: string | null = null;
-
+  fileInBase64: string
   constructor(
+
     private formBuilder: FormBuilder,
     private uploadService: UploadService
   ) {
@@ -35,33 +34,40 @@ export class CreateProjectComponent {
     });
   }
 
-  onFileChange(event: any) {
-    if (event.target && event.target.files && event.target.files.length > 0) {
-      this.file = event.target.files[0];
-      this.selectedFileName = this.file ? this.file.name : null;
+  async onFileChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+
+    if (inputElement && inputElement.files && inputElement.files.length > 0) {
+
+      // instancia leitor de arquivos
+      const fileRender = new FileReader()
+      const fileName = inputElement.files[0].name
+
+      // le a imagem de files 
+      fileRender.readAsDataURL(inputElement.files[0])
+
+      // onload recebe uma callback quando a imagem for carregada. o fileRender.result contem a base64 em string
+      fileRender.onload = () => this.fileInBase64 = fileRender.result!.toString()
+
+      // salva o nome da imagem pra rendenizar no front
+      this.selectedFileName = fileName
     } else {
       console.log("No files selected");
-      this.file = null;
       this.selectedFileName = null;
     }
   }
-  
 
-  upload() {
-    if (this.createProjetForm.valid && this.file) {
+  async upload() {
+    if (this.createProjetForm.valid && this.fileInBase64) {
       const title = this.createProjetForm.get('title')!.value;
       const description = this.createProjetForm.get('description')!.value;
 
-      this.file.arrayBuffer().then(arrayBuffer => {
-        const base64String = btoa(String.fromCharCode(...new Uint8Array(arrayBuffer)));
-        const image = `data:${this.file?.type};base64,${base64String}`
-        this.uploadService.uploadFile(title, description, image).subscribe((resp) => {
-          alert('Uploaded');
-        });
-      })
+      this.uploadService.uploadFile(title, description, this.fileInBase64).subscribe((resp) => {
+        alert('Uploaded');
+      });
 
     } else {
       alert("Please select a file first");
     }
-  }  
+  }
 }
