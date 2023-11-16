ad453b6dbe8f34c95937d9f5e46156f9e298bd74
abressam <amandabressam@gmail.com>
2023-11-16 00:27:38 -0300

    feat: added curriculum in profile

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UploadService } from 'src/app/service/project/upload.service';
+import { CurriculumService } from 'src/app/service/curriculum/curriculum.service';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-profile',
@@ -8,14 +9,16 @@ import { Router } from '@angular/router';
 })
 export class ProfileComponent {
   public projects: any[] = [];
+  public curriculum: any
 
   constructor(
     private uploadService: UploadService,
+    private curriculumService: CurriculumService,
     private router:Router) {}
 
   ngOnInit(): void {
     this.getProjects();
-    console.log(this.getProjects())
+    this.getCurriculum();
   }
 
   private getProjects(): void {
@@ -27,6 +30,18 @@ export class ProfileComponent {
         console.error('Erro ao obter dados dos projetos:', error);
       }
     );
+  }
+
+  private getCurriculum(): void {
+    this.curriculumService.getCurriculum().subscribe(
+      (curriculum: any) => {
+        this.curriculum = curriculum;
+        console.log(curriculum)
+      },      
+      (error) => {
+        console.error('Erro ao obter dados do curriculo:', error);
+      }
+    )
   }
 
   public redirectCreateProject() {
