a60ef40e00387d1201a9e36f5e22d9863b8ac12a
abressam <amandabressam@gmail.com>
2023-11-18 13:13:36 -0300

    feat: added createMessage

--- src/app/components/feed-dialog/feed-dialog.component.ts
+++ src/app/components/feed-dialog/feed-dialog.component.ts
@@ -5,6 +5,8 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { Validators, FormsModule, ReactiveFormsModule, FormGroup, FormBuilder } from '@angular/forms';
+import { FeedService } from 'src/app/service/feed/feed.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-feed-dialog',
@@ -16,24 +18,45 @@ import { Validators, FormsModule, ReactiveFormsModule, FormGroup, FormBuilder } 
 export class FeedDialogComponent {
   feedForm: FormGroup;
 
-  constructor(    
+  constructor(
+    private feedService: FeedService,    
     private formBuilder: FormBuilder,
+    private router: Router,
     public dialogRef: MatDialogRef<FeedDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {
     this.feedForm = this.formBuilder.group({
-      description: ['', [Validators.required]],
+      message: ['', [Validators.required]],
     });
   }
 
-  get descriptionControl() {
-    return this.feedForm.get('description');
+  get messageControl() {
+    return this.feedForm.get('message');
   }
 
   get characterCount() {
-    const currentLength = this.descriptionControl?.value ? this.descriptionControl.value.length : 0;
-    const maxLength = this.descriptionControl?.getError('maxlength')?.requiredLength || 900;
+    const currentLength = this.messageControl?.value ? this.messageControl.value.length : 0;
+    const maxLength = this.messageControl?.getError('maxlength')?.requiredLength || 900;
     return `${currentLength}/${maxLength}`;
+  }
+
+  async createMessage() {
+    if(!this.feedForm) {
+      alert('O campo não deve ser vazio')
+    } else {
+      const message = this.feedForm.get('message')!.value
+
+      console.log(message)
+
+      this.feedService.createMessage(message).subscribe(() => {
+        alert('Mensagem enviada com sucesso!')
+        
+        this.router.navigate(['/home'])
+        .then(() => {
+          window.location.reload()
+        })
+      })
+    }
   }
 
 }
